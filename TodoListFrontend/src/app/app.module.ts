2ba464e93fb01124995ab0161bb7beabc2903b30
Rohan <rohankangutkar170594@gmail.com>
2019-10-02 22:10:55 +0530

    Added login and signup component routes - rohan

--- TodoListFrontend/src/app/app.module.ts
+++ TodoListFrontend/src/app/app.module.ts
@@ -4,13 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// routing
+import { RouterModule, ROUTES } from '@angular/router';
+import { UsersModule } from './users/users.module';
+import { LoginComponent } from './users/login/login.component';
+import { HttpClientModule } from '@angular/common/http'
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { ToastrModule } from 'ngx-toastr';
+
+//toast message 
+// import {}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    ToastrModule.forRoot(),
+    UsersModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent, pathMatch: 'full'},
+      {path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '*', component: LoginComponent },
+      { path: '**', component: LoginComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TodoListFrontend/src/app/users/users.module.ts
+++ TodoListFrontend/src/app/users/users.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { ToastrModule } from 'ngx-toastr';
 
 
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    RouterModule.forChild([
+      {path:'signup', component: SignupComponent}
+    ])
   ]
 })
 export class UsersModule { }
