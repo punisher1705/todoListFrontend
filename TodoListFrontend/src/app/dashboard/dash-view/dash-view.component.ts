74c624c1bc2f76b7a8a3a62355ca5bb8d62bc0e5
Rohan <rohankangutkar170594@gmail.com>
2019-11-12 02:22:32 +0530

    Changes made by rohan

--- TodoListFrontend/src/app/dashboard/dash-view/dash-view.component.ts
+++ TodoListFrontend/src/app/dashboard/dash-view/dash-view.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cookie } from 'ng2-cookies/ng2-cookies';
 import { Router } from '@angular/router';
-
-//chart
 import { ChartType, ChartOptions } from 'chart.js';
-import { Label } from 'ng2-charts';
-import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
 
 @Component({
   selector: 'app-dash-view',
@@ -15,68 +12,27 @@ import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 export class DashViewComponent implements OnInit {
 
   private username: string;
-
-  //pie chart
+  
+  // Pie
   public pieChartOptions: ChartOptions = {
     responsive: true,
-    legend: {
-      position: 'top',
-    },
-    plugins: {
-      datalabels: {
-        formatter: (value, ctx) => {
-          const label = ctx.chart.data.labels[ctx.dataIndex];
-          return label;
-        },
-      },
-    }
   };
   public pieChartLabels: Label[] = ['Due', 'Doing', 'Done'];
-  public pieChartData: number[] = [300, 500, 100];
+  public pieChartData: SingleDataSet = [300, 500, 100];
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
-  public pieChartPlugins = [pluginDataLabels];
-  public pieChartColors = [
-    {
-      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
-      // color: ['rgb(255,255,255)']
-    },
-  ];
-
- //pie end
+  public pieChartPlugins = [];
 
   constructor(
     private router: Router
-  ) { }
+  ) { 
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
+  }
 
   ngOnInit() {
     this.username = Cookie.get('username');
     console.log(this.username)
   }
-
-  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
-
-  // addSlice() {
-  //   this.pieChartLabels.push(['Line 1', 'Line 2', 'Line 3']);
-  //   this.pieChartData.push(400);
-  //   this.pieChartColors[0].backgroundColor.push('rgba(196,79,244,0.3)');
-  // }
-
-  // removeSlice() {
-  //   this.pieChartLabels.pop();
-  //   this.pieChartData.pop();
-  //   this.pieChartColors[0].backgroundColor.pop();
-  // }
-
-  // changeLegendPosition() {
-  //   this.pieChartOptions.legend.position = this.pieChartOptions.legend.position === 'left' ? 'top' : 'left';
-  // }
-
-  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
-
 
 }
